b0a79644e83f45806556efa2898d5840ac39676d
whoami847 <mrbeastbangla069@gmail.com>
2025-04-06 04:50:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from bot import Bot
+# bot.py
+from flask import Flask
 
+class Bot:
+    def __init__(self):
+        self.app = Flask(__name__)
+        
+    def run(self):
+        self.app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+# main.py
+from bot import Bot
 Bot().run()
