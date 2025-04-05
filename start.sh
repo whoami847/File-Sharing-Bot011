145adaa2426b999b72408e7a77de196786c7fadc
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 18:00:41 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/whoami847/File-Sharing-Bot011 /File-Sharing-Bot011
 cd /MissPerfectURLBOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python3 main.py
