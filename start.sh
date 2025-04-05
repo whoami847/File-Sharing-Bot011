e5a4e420dc166de94e609beb325b9cc15c77bf32
whoami847 <mrbeastbangla069@gmail.com>
2025-04-05 17:59:20 +0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/whoami847/File-Sharing-Bot011 /File-Sharing-Bot011
+cd /MissPerfectURLBOT
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
